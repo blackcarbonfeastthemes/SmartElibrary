6e952afaef0f3949cd64496484d822a9f8e1b6c0
Amila <sales@blackcarbonfeast.com>
2019-06-07 15:07:47 +0530

    category

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -70,10 +70,8 @@
               </li>
             </ul>
           </li>
-          <!-- Control Sidebar Toggle Button -->
-          <li>
-            <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
-          </li>
+        
+
         </ul>
       </div>
     </nav>
